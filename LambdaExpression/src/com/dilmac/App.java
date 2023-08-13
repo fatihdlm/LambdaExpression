5d8e73ffb99bea1717617d9a7f61007838060c92
fdilmac <fatihdlm@hotmail.com>
2023-08-13 12:26:38 +0300

    Anonymous version has been impelemented.

--- LambdaExpression/src/com/dilmac/App.java
+++ LambdaExpression/src/com/dilmac/App.java
@@ -4,9 +4,10 @@ public class App {
 
 	public static void main(String[] args) {
 		/**
-		 *  There two way to use interface method.
+		 *  There three ways to implement body of declared method in interface.
 		 *  1. Traditional way
-		 *  2. Using Lambda Expression
+		 *  2. Using Anonymous Class
+		 *  3. Using Lambda Expression
 		 */
 		
 		/**
@@ -17,6 +18,23 @@ public class App {
 		 */
 		LambdaImp lambdaImp=new LambdaImp();
 		lambdaImp.display("This is an example of Traditional way to use implemented abstract method");
+		
+		System.out.println("****************************************************");
+		/**
+		 * 2. Using Anonymous class to impelement abstract method without extrac implemantation class
+		 * First we need to create an instance of anonymous class with the implementation of the method.
+		 */
+		ILambdaExpression anonymousObj=new ILambdaExpression() {
+			
+			@Override
+			public void display(String message) {
+				System.out.println("This method is an implementation of interface method\nMessage :"+message);
+				
+			}
+		};
+		
+		anonymousObj.display("This is an example of Using Anonymous Class to  implement and use abstract method");
+	
 	
 	}
 
