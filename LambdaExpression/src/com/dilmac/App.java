16d31be0c98e33a3aceee86f7a85f8a514e20482
fdilmac <fatihdlm@hotmail.com>
2023-08-13 12:20:16 +0300

    The traditional way to use abstract method implemented.

--- LambdaExpression/src/com/dilmac/App.java
+++ LambdaExpression/src/com/dilmac/App.java
@@ -3,9 +3,21 @@ package com.dilmac;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("I will use lambda expression in this example");
+		/**
+		 *  There two way to use interface method.
+		 *  1. Traditional way
+		 *  2. Using Lambda Expression
+		 */
+		
+		/**
+		 * 1. Traditional way to use implemented method.
+		 * First we need to declare a class (in our case LambdaImp.java) that implements an interface
+		 * Then we have to implement the body of abstract method(s).
+		 * --- Usage: create an instance of class then call the method(which is display).
+		 */
+		LambdaImp lambdaImp=new LambdaImp();
+		lambdaImp.display("This is an example of Traditional way to use implemented abstract method");
+	
 	}
 
 }
--- LambdaExpression/src/com/dilmac/LambdaImp.java
+++ LambdaExpression/src/com/dilmac/LambdaImp.java
@@ -3,7 +3,7 @@ package com.dilmac;
 public class LambdaImp implements ILambdaExpression {
 	@Override
 	public void display(String message) {
-		System.out.println("This method is an implementation of interface method\n Message :"+message);
+		System.out.println("This method is an implementation of interface method\nMessage :"+message);
 		
 	}
 
